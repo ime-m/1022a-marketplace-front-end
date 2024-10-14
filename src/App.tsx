9668d6e72ed3dd799ef0c4e36ec1099bf4261224
ime-m <emily.cruz@estudante.ifms.edu.br>
2024-10-14 14:28:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-type ProdutoType ={
-  id:number,
-  nome:string,
-  preco:string,
-  descricao:string,
-  imagem:string
+type ProdutoType = {
+    id:number,
+    nome:string,
+    preco:string,
+    descricao:string,
+    imagem:string
 }
-
-
 function App() {
   const [nome, setNome] = useState("")
-  const [produtos, setProdutos] = useState<ProdutoType[]>([])
-useEffect(()=>{
-  setNome("Emily Marques da Cruz")
-  fetch("https://one022a-marketplace-q5t4.onrender.com/produtos")
-  .then(resposta=>resposta.json())
-  .then(dados=>setProdutos(dados))
-
-},[])
+  const [produtos, setProdutos] = useState<ProdutoType []>([])
+  //useEffects(O que fazer, quando Fazer) []=> Hora do carregamento da página
+  useEffect(() => {
+    setNome("Guilherme Terenciani")
+    //Buscar os dados do BackENd
+    fetch("https://one022a-marketplace-e90o.onrender.com/produtos")
+      .then(resposta => resposta.json())
+      .then(dados => setProdutos(dados))
+    //Colocar em uma variável
+  }, [])
 
   return (
     <>
-    <h1>{nome}</h1>
-    <div className="produtos-container">
-    {
-      produtos.map(produtos =>{
+      <h1>{nome}</h1>
+      <div className="produtos-container">
+        {
+          produtos.map(produto => {
+            return (
+              <div key={produto.id} className="produto-item">
+                <h1>{produto.nome}</h1>
+                <img src={produto.imagem} alt="Imagem do celular" />
+                <p>{produto.preco}</p>
+                <p>{produto.descricao}</p>
+              </div>
+            )
+          })
+        }
 
-        return(
-          <div className="produtos=item">
-          <h1>{produtos.nome}</h1>
-          <p>{produtos.imagem}</p>
-          <p>{produtos.preco}</p>
-          <p>{produtos.descricao}</p>
-  </div>
-         )
-})
-    }
-    </div>
+      </div>
     </>
-  }
-
+  )
+}
 
 export default App
